423553305c00e445474338606b81486e6a24698b
davidquintr <davidquintr97@gmail.com>
2023-06-14 15:22:56 -0600

    trying to fix it

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/portfolio',
   images: {
     domains: ['davidquintr.github.io'],
     unoptimized: true
