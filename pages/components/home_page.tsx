981eadf5c2a1a0af04e7809b2ebb385a4797cdb7
davidquintr <davidquintr97@gmail.com>
2023-06-14 13:48:57 -0600

    update

--- pages/components/home_page.tsx
+++ pages/components/home_page.tsx
@@ -1,10 +1,9 @@
 import Head from "next/head";
 import PersonInit from "./person_init";
-import { NavBar } from "../nav_bar";
+import NavBar from "../nav_bar";
 import { PageWrapper } from "./page_wrapper";
 import AboutMe from "./about_me";
 import PersonalInfo from "./personal_info";
-
 
 export const HomePage = () => {
   return (
