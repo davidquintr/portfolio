0125a85c7795514cde08274f9c2efb770cc9b041
davidquintr <davidquintr97@gmail.com>
2023-06-14 16:34:12 -0600

    trying to put buttons 2

--- pages/components/project_item.tsx
+++ pages/components/project_item.tsx
@@ -40,7 +40,7 @@ const ProjectsItem: React.FC<ProjectsItemProps> = ({ element, index }) => {
           </div>
           <div className="project-buttons">
           {element?.urlView != "" ? (
-              <Link href={"url"} className="button-link extended light" target="__blank">
+              <Link href={element?.urlView} className="button-link extended light" target="__blank">
                 <FontAwesomeIcon
                   icon={faEye}
                   color="fff"
