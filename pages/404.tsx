405a7af10882c2688ad34eb0a84fce68eb33afc2
davidquintr <davidquintr97@gmail.com>
2023-06-30 11:54:29 -0600

    error message and error message

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,3 @@
 export default function Custom404() {
-  return <h1>404 - Not found</h1>
+  return <h1 className="error-text">404 - Not found</h1>
 }
--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -1,15 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
 
 const Footer = () => {
     return(
         <>
             <footer className="footer">
-                <Link href={"https://github.com/davidquintr/portfolio"}>
-                    <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-                </Link>
                 <p>©2023 Designed and Programmed by David Quintanilla</p>
+                <div className="footer-social">
+                    <Link href={"https://github.com/davidquintr/portfolio"}>
+                        <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                    </Link>
+                    <Link href={"https://www.linkedin.com/in/david-quintanilla-ruiz-a41044231/"}>
+                        <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                    </Link>
+                </div>
             </footer>
         </>
     )
