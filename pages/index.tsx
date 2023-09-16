f6a64680ed4f6c88d0fbb0152399d4995dacf9d8
davidquintr <davidquintr97@gmail.com>
2023-09-16 13:18:31 -0600

    si

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,6 @@ export default function Home() {
         <meta property="og:title" content="Portfolio - David Quintanilla"></meta>
         <meta property="og:description" content="Hello! I'm David Quintanilla Ruiz, a programmer who also dabbles in the world of design. I consider myself a fast learner, easily adaptable to the current needs."></meta>
         <meta property="og:image" content="https://davidquintr.github.io/portfolio/thumbnail.png"></meta>
-
         <meta name="twitter:card" content="summary_large_image"></meta>
         <meta property="twitter:url" content="https://davidquintr.github.io/portfolio/"></meta>
         <meta name="twitter:title" content="Portfolio - David Quintanilla"></meta>
