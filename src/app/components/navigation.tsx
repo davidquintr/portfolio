56d4467ebe59383428694293c9626fa6bf9bb1d5
davidquintr <davidquintr97@gmail.com>
2023-12-29 00:52:12 -0600

    changed links to routes

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,43 +1,21 @@
 "use client";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import en from "../sources/en";
-import {
-  faAddressBook,
-  faHouse,
-  faBarsProgress,
-  faBlog,
-} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-const links = [
-  { href: "/", text: en.navigation.home, icon: faHouse },
-  { href: "/blog", text: en.navigation.blog, icon: faBlog },
-  {
-    href: "/projects",
-    text: en.navigation.projects,
-    icon: faBarsProgress,
-  },
-  {
-    href: "/contact",
-    text: en.navigation.contact,
-    icon: faAddressBook,
-  },
-];
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import routes from "../sources/routes";
 
 export default function Navigation() {
   const path = usePathname();
 
   return (
     <>
-      {links.map((link, index) => {
+      {routes.map((link, index) => {
         return (
           <Link
             key={index}
             className={`flex h-8 hover:bg-light-gray text-light-blue-500 border-light-blue-500 gap-1 sm:gap-1.5 justify-center sm:h-11 items-center w-36 dark:bg-dark-blue-500 rounded-full dark:hover:bg-dark-blue-200 transition-all active:scale-95 dark:text-dark-blue-100 ${
               link.href == path &&
-              "border !bg-light-gray hover:!bg-white dark:border-dark-blue-100 dark:!bg-dark-blue-200 dark:hover:!bg-dark-blue-500"
+              "border !bg-light-gray hover:bg-white dark:border-dark-blue-100 dark:!bg-dark-blue-200 dark:hover:!bg-dark-blue-500"
             }`}
             href={link.href}
           >
---
+++ src/app/sources/routes.tsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import en from "../sources/en";
+import {
+  faAddressBook,
+  faHouse,
+  faBarsProgress,
+  faBlog,
+} from "@fortawesome/free-solid-svg-icons";
+
+const routes = [
+  { href: "/", text: en.navigation.home, icon: faHouse },
+  { href: "/blog", text: en.navigation.blog, icon: faBlog },
+  {
+    href: "/projects",
+    text: en.navigation.projects,
+    icon: faBarsProgress,
+  },
+  {
+    href: "/contact",
+    text: en.navigation.contact,
+    icon: faAddressBook,
+  },
+];
+
+export default routes;
