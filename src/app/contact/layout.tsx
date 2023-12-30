d05346e09435c664266cf486dd1b99ba83d7b358
davidquintr <davidquintr97@gmail.com>
2023-12-30 02:48:20 -0600

    open graph added

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+import Page from "./page";
+import { MetadataContact } from "../sources/metadata_en";
+
+export const metadata: Metadata = MetadataContact;
+
+export default Page;
