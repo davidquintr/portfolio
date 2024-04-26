f844bfca81df04ef776c8977b394da25b0420157
davidquintr <davidquintr97@gmail.com>
2024-04-26 12:48:06 -0600

    xd

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export function middleware(request: NextRequest) {
 
   const isSpanish = request.headers.get('accept-language')?.split(';')[0].includes('es');
   const isPostPath = pathname.startsWith('/post/')
-  const isPublic = ['/blog/','/documents/','/experience/','/projects/', '/thumbnail', '/icon', 'davidquint'].some(path => pathname.startsWith(path))
+  const isPublic = ['/blog/','/documents/','/experience/','/projects/', '/thumbnail', '/icon', '/davidquint'].some(path => pathname.startsWith(path))
  
   if (pathnameHasLocale || isPostPath || isPublic) return
  
