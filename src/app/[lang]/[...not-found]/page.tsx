6599e70a6dc9466976396665409762b8c4112b1c
davidquintr <davidquintr97@gmail.com>
2024-04-27 15:25:10 -0600

    thumbnail to not found

--- src/app/[lang]/[...not-found]/page.tsx
+++ src/app/[lang]/[...not-found]/page.tsx
@@ -1,4 +1,7 @@
 import NotFoundCat from "@/app/components/not_found_cat";
+import { getDictionary } from "../dictionaries";
+import { Metadata } from "next";
+import createPageMetadata from "@/app/sources/metadata";
 
 export default function NotFound({
   params: { lang },
@@ -9,4 +12,21 @@ export default function NotFound({
   return (
     <NotFoundCat type="pages" lang={lang} />
   );
+}
+
+export async function generateMetadata({
+  params: { lang },
+}: {
+  params: { lang: string };
+}): Promise<Metadata> {
+  const dict = getDictionary(lang);
+
+  return createPageMetadata({
+    title: dict.not_found.joke + ' 404',
+    thumbnail: 'thumbnail-404.webp',
+    page: {
+      title: dict.not_found.joke + ' 404',
+      description: '',
+    },
+  });
 }
--- src/app/post/[slug]/not-found.tsx
+++ src/app/post/[slug]/not-found.tsx
@@ -1,7 +1,23 @@
+import { getDictionary } from "@/app/[lang]/dictionaries";
 import NotFoundCat from "@/app/components/not_found_cat";
+import createPageMetadata from "@/app/sources/metadata";
+import { Metadata } from "next";
 
 export default function NotFound() {
   return (
     <NotFoundCat lang="en" type="blog" />
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const dict = getDictionary('en');
+
+  return createPageMetadata({
+    title: dict.not_found.joke + ' 404',
+    thumbnail: dict.not_found.thumbnail,
+    page: {
+      title: dict.not_found.joke + ' 404',
+      description: '',
+    },
+  });
 }
