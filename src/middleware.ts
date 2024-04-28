b8fc38901c714b6735473a341b4317093df83f5a
davidquintr <davidquintr97@gmail.com>
2024-04-28 15:24:46 -0600

    enabled sitemap and robots

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export function middleware(request: NextRequest) {
 
   const isSpanish = request.headers.get('accept-language')?.split(';')[0].includes('es');
   const isPostPath = pathname.startsWith('/post/')
-  const isPublic = ['/blog/','/documents/','/experience/','/projects/', '/thumbnail', '/icon', '/davidquint', '/shh'].some(path => pathname.startsWith(path))
+  const isPublic = ['/blog/','/documents/','/experience/','/projects/', '/thumbnail', '/icon', '/davidquint', '/shh', '/robots', '/sitemap'].some(path => pathname.startsWith(path))
  
   if (pathnameHasLocale || isPostPath || isPublic) return
  
